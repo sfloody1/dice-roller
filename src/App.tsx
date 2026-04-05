d5483c5413d8774de0a37485b98651bdc948a963
sfloody1 <sfloody1@jhu.edu>
2026-04-05 18:47:33 -0400

    dice-05: Add interactivity with useState

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-8 bg-green-700">
       <h1 className="text-4xl font-bold text-white">Dice Roller</h1>
-      <Die value={1} />
+      <Die initialValue={1} />
+      <Die initialValue={5} />
     </div>
   );
 }
