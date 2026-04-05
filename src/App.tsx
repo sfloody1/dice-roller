35b1edf7f7951ef8206338956312e8df75e85ca6
sfloody1 <sfloody1@jhu.edu>
2026-04-05 18:43:29 -0400

    dice-04: Make Die configurable with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Die from "./components/Die";
 
+
 function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-8 bg-green-700">
       <h1 className="text-4xl font-bold text-white">Dice Roller</h1>
-      <Die />
+      <Die value={1} />
     </div>
   );
 }
